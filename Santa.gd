f4a2741881c0192c31f1398526bc6b9315544455
Darian | Praxis <hello@vulpinecat.com>
2018-12-19 17:24:55 +0100

    adding offset of idle / walk animation

--- Santa.gd
+++ Santa.gd
@@ -2,26 +2,68 @@ extends Area2D
 
 var standard_sprite_size
 var sprite_offset_direction
+var walking = false
+var prev_anim
 
 func _ready():
 	# Connecting here to prevent debug errors in the editor
 	$Sprite.connect("frame_changed", self, "_on_Sprite_frame_changed")
-	standard_sprite_size = $Sprite.frames.get_frame('idle', 1).get_size()
+	standard_sprite_size = $Sprite.frames.get_frame('idle', 0).get_size()
 	
 	# This means it'll push up and right when the sprite is growing
 	sprite_offset_direction = Vector2(1, -1)
 	
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		$Sprite.flip_h = false
+		
+	
+	if Input.is_action_just_pressed("ui_left"):
+		$Sprite.flip_h = true
+	pass
 
 
 func _on_Sprite_frame_changed():
-	var current_loop = $Sprite.animation
-	var current_index = $Sprite.frame
-	var current_sprite_size = $Sprite.frames.get_frame(current_loop, current_index).get_size()
+	switch_sprite_offset($Sprite.animation)
 	
-	$Sprite.offset = (current_sprite_size - standard_sprite_size) * sprite_offset_direction
+	
+func switch_sprite_offset(anim):
+	match anim:
+		"idle":
+			sprite_idle_offset(anim)
+		"walk":
+			sprite_walk_offset(anim)
+		_:
+			pass
+
+func sprite_idle_offset(idle):
+	match [$Sprite.frame, $Sprite.flip_h]:
+		[3, false], [4, false]:
+			$Sprite.offset = Vector2(0, 1)
+		[4, true]:
+			$Sprite.offset = Vector2(-1, 1)
+		[3, true]:
+			$Sprite.offset = Vector2(-1, 1)
+		[_, false]:
+			$Sprite.offset = Vector2(0, 0)
+		[_, true]:
+			$Sprite.offset = Vector2(-1, 0)
+	
+
+func sprite_walk_offset(walk):
+	match $Sprite.frame:
+		0, 2, 4, 6:
+			$Sprite.offset = Vector2(0, -1)
+		1, 5:
+			$Sprite.offset = Vector2(0, -2)
+		_:
+			$Sprite.offset = Vector2(0, 0)
+
+
+	
+func store():
+	pass
+
+	
 
